8fa71d230afd22174ab331b24373a465d7d6988e
Wef2 <qhrud5725@hanmail.net>
2016-07-15 17:33:57 +0900

    Add Activities

--- NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/MainActivity.java
+++ NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/MainActivity.java
@@ -1,13 +1,38 @@
 package mcl.jejunu.naturaldyeing;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button meaningButton, historyButton, colorButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        meaningButton = (Button) findViewById(R.id.meaning_button);
+        historyButton = (Button) findViewById(R.id.history_button);
+        colorButton = (Button) findViewById(R.id.color_button);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.meaning_button:
+                startActivity(new Intent(MainActivity.this, MeaningActivity.class));
+                break;
+            case R.id.history_button:
+                startActivity(new Intent(MainActivity.this, HistoryActivity.class));
+                break;
+            case R.id.color_button:
+                startActivity(new Intent(MainActivity.this, ColorActivity.class));
+                break;
+        }
+
     }
 }
---
+++ NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/MeaningActivity.java
@@ -0,0 +1,13 @@
+package mcl.jejunu.naturaldyeing;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MeaningActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meaning);
+    }
+}
