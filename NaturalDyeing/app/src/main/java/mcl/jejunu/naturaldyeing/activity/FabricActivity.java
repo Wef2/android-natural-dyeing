36c6184ba5878cbc5bf56628f2966f119028e8a6
Wef2 <qhrud5725@hanmail.net>
2016-08-01 17:01:45 +0900

    Update FabricActivity, ProductActivity

--- NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/activity/FabricActivity.java
+++ NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/activity/FabricActivity.java
@@ -1,15 +1,28 @@
 package mcl.jejunu.naturaldyeing.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import mcl.jejunu.naturaldyeing.R;
 
 public class FabricActivity extends AppCompatActivity {
 
+    private Button productButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_fabric);
+
+        productButton = (Button) findViewById(R.id.product_button);
+        productButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(FabricActivity.this, ProductActivity.class));
+            }
+        });
     }
 }
--- NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/activity/ProductActivity.java
+++ NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/activity/ProductActivity.java
@@ -10,6 +10,6 @@ public class ProductActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cloth);
+        setContentView(R.layout.activity_product);
     }
 }
