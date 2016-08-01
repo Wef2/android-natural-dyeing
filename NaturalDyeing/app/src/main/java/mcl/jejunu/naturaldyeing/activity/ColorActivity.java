af37b20f20c2e8498f9eafc13779de6f411b0414
Wef2 <qhrud5725@hanmail.net>
2016-08-01 16:41:26 +0900

    Update ColorActivity

--- NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/activity/ColorActivity.java
+++ NaturalDyeing/app/src/main/java/mcl/jejunu/naturaldyeing/activity/ColorActivity.java
@@ -2,14 +2,19 @@ package mcl.jejunu.naturaldyeing.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 import mcl.jejunu.naturaldyeing.R;
 
 public class ColorActivity extends AppCompatActivity {
 
+    private Button circleButton;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_color);
+
+        circleButton = (Button) findViewById(R.id.circle_button);
     }
 }
